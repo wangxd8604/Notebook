473924394d4af30e60d6ceb70431830f458c5a26
wangxd8604 <164854184+wangxd8604@users.noreply.github.com>
2025-07-01 13:46:47 +0800

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "wangxd8604.github.io/Notebook"",
+    baseUrl: "wangxd8604.github.io/Notebook",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
